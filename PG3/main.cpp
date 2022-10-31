b38b18d93520c3d248a1bebd9ecfee74010a8014
ソンカイヨウ <k021g1268@g.neec.ac.jp>
2022-10-31 12:46:28 +0900

    タイムアウト関数を使用した

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -7,6 +7,9 @@ using namespace std;
 // 乱数を取得する関数
 int GetRange(const int& min, const int& max);
 
+// 奇数か偶数を判断する関数
+int DecideOddOrEven(const int& num);
+
 int main()
 {
 	srand(time(nullptr));
@@ -14,39 +17,25 @@ int main()
 	int num = 0;
 	cin >> num;
 
-	// 三秒スリープする
-	Sleep(3000);
-
-	// 奇数か偶数を判断するラムダ式関数
-	function<int(const int&)> fx = [](const int& num)
+	// 抽選結果を返す関数
+	function <void()> Lottry = [=]()
 	{
-		if (num % 2 == 0)
-		{
-			return 0;	// 偶数
-		}
-		else if (num % 1 == 0)
-		{
-			return 1;	// 奇数
-		}
-		else
-		{
-			return -1;	// エラー
-		}
+		// サイコロの出目
+		int randNum = GetRange(0, 6);
+		cout << "抽選結果 :" << randNum << endl;
+		DecideOddOrEven(num) == DecideOddOrEven(randNum) ? cout << "当たり！" << endl : cout << "はずれ！" << endl;
 	};
 
-	// サイコロの出目
-	int randNum = GetRange(0, 6);
-	cout << "抽選結果 :" << randNum << endl;
-
-	if (fx(num) == fx(randNum))
+	// タイムアウト関数
+	function<void(function<void()>, const int&)> SetTimeOut =
+		[](function<void()> fx, const int& second)
 	{
-		cout << "当たり！" << endl;
-	}
-	else
-	{
-		cout << "はずれ！" << endl;
-	}
+		Sleep(second * 1000);
+		fx();
+	};
 
+	// 実行
+	SetTimeOut(Lottry, 3);
 	return 0;
 }
 
@@ -54,4 +43,20 @@ int main()
 int GetRange(const int& min, const int& max)
 {
 	return rand() % ((max - min) + 1) + min;
+}
+
+int DecideOddOrEven(const int& num)
+{
+	if (num % 2 == 0)
+	{
+		return 0;	// 偶数
+	}
+	else if (num % 1 == 0)
+	{
+		return 1;	// 奇数
+	}
+	else
+	{
+		return -1;	// エラー
+	}
 }
